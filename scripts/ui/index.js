6b74f063829190f6d894a4889425b4e80b4c78b1
gitmeserver <gitmeserver@gmail.com>
2016-02-19 18:08:30 +0900

    Added files via upload

--- scripts/ui/index.js
+++ scripts/ui/index.js
@@ -103,8 +103,7 @@ function thumbnail(contents, channelFileNo){
 	var contentsId = $(contents).find("contentsId").text();
 	var thumbTitle = $(contents).find("title").text();
 	
-//	var th = $.parseHTML("<div class='cont col-xs-6 col-lg-2'></div>");
-	var th = $.parseHTML("<div class='cont'></div>");
+	var th = $.parseHTML("<div class='cont col-xs-6 col-lg-2'></div>");
 	$(th).attr("style", "text-align:center; padding-bottom:10px;");
 	
 	var imgA = $.parseHTML("<a href='contents.html?contentsId=" + contentsId + "&amp;channelFileNo=" + channelFileNo + "'></a>");
