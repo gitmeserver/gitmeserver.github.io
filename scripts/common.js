0dc5d6ce30a3c832b91f6d512c3c31b3f427d0de
DevY <hanys@mbuzzer.co.kr>
2016-02-29 16:08:09 +0900

    1

--- scripts/common.js
+++ scripts/common.js
@@ -380,6 +380,9 @@ $.extend({
 	},
 	
 	getDeck : function(channeldId, contentsId){
+		
+		console.log($.cookie("deckList"));
+		
 		var deckList = $.cookie("deckList").split(",");
 		for(var i=0; i<deckList.length; i++){
 			if(-1 < deckList[i].indexOf(channelId + "_" + contentsId + "_")){
