eb758071b9b98d6ad3162ffa50907351d95e88dc
DevY <hanys@mbuzzer.co.kr>
2016-03-01 17:00:54 +0900

    1

--- scripts/subtitle.js
+++ scripts/subtitle.js
@@ -1,14 +1,17 @@
 /**
  * 
  */
+var document;
+
 function getSubtitle(){
 	$.ajax({
 		type: 'get'
 		, dataType: "text"
 		, url: "http://devys.github.io/subtitle/kungfu.ko.smi"
 		, success: function(data) {
-//			console.log(data);
-			alert(data);
+			document = $.parseXML(data);
+			var syncs = $(document).find("sync");
+			console.log(syncs.length);
 		}
 		, error: function(xhr, status, error) {
 			console.log("error");
