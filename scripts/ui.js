907747ad44d9e290e3ce35d0de1ff9aa7b8c992b
DevY <DevY@172.30.125.15>
2016-03-04 11:14:23 +0900

    1

--- scripts/ui.js
+++ scripts/ui.js
@@ -399,7 +399,7 @@ function video(){
 	$("#video").append("<div id='subject'></div>");
 	$("#subject").append("<h3></h3><span></span>");
 	$("#video").append("<div id='embed' class='embed-responsive embed-responsive-16by9'></div>");
-	$("#embed").append("<button id='fullscreenOff' type='button' class='glyphicon glyphicon-resize-small btn btn-default btn-lg'></button>");
+	$("#embed").append("<div id='fullscreenOff'><button type='button' class='glyphicon glyphicon-resize-small btn btn-default btn-lg'></button><div>");
 	$("#embed").append("<video id='player' controls='true' autoplay='true' class='embed-responsive-item'></video>");
 	$("#embed").append("<div class='srt' data-video='player' data-srt='http://devys.github.io/subtitle/kungfu.ko.srt'></div>");
 	$("#detail").append("<div id='controll'><div class='float-right'></div><span></span></div></div>");
