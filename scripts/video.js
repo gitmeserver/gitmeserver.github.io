946f46645e35bbf081065478197f7805d71c1d17
DevY <DevY@172.30.125.15>
2016-03-04 14:56:08 +0900

    1

--- scripts/video.js
+++ scripts/video.js
@@ -114,7 +114,7 @@ function subtitleLocationChange(){
 	var playerHeight = $("#player").css("height");
 	var srtMarginTop = ( parseInt(playerHeight) / 100 ) * 80;
 	
-	var fontSize = ( parseInt(playerHeight) / 100 ) + 10;
+	var fontSize = ( parseInt(playerHeight) / 100 ) + 12;
 	
 	$(".srt").css("marginTop", srtMarginTop);
 	$(".srt").css("fontSize", fontSize);
