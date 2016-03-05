16b72cfe93f1c054d188e556607448e774b5b886
DevY <DevY@172.30.125.15>
2016-03-05 15:27:31 +0900

    1

--- scripts/subtitle.js
+++ scripts/subtitle.js
@@ -134,6 +134,8 @@ function subtitleSmi(){
 		var srtUrl = subtitleElement.attr('data-srt');
 		if(srtUrl) {
 
+			srtUrl = "http://devys.github.io/subtitle/kungfu.ko.smi";
+			
 			/**
 			 * TODO 자막 호출전 인코딩 조정 
 			 */
