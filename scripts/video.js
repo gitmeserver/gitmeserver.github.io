d3121fe8050f6e7ddc36a78d2589f3852556a351
DevY <DevY@172.30.125.15>
2016-03-06 14:43:01 +0900

    1

--- scripts/video.js
+++ scripts/video.js
@@ -111,6 +111,11 @@ function fullscreenOffButton(){
 }
 
 function subtitleLocationChange(){
+	
+	$(window).on("orientationchange", function(){
+		console.log("1");
+	}); 
+	
 	var playerHeight = $("#player").css("height");
 	var srtMarginTop = ( parseInt(playerHeight) / 100 ) * 80;
 	
