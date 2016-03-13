2056e651f165bcd2bf3a60b54187bde5c32caebc
DevY <hanys1042@naver.com>
2016-03-13 18:36:36 +0900

    1

--- scripts/ui.js
+++ scripts/ui.js
@@ -416,7 +416,7 @@ function video(){
 	$("#videoControll").append("<button id='save' type='button' class='glyphicon glyphicon-floppy-disk btn btn-default'></button>");
 	$("#videoControll").append("<button id='previous' type='button' style='display:none;' class='glyphicon glyphicon-facetime-video btn btn-default'></button>");
 	
-	$("#videoControll").append("<div id='controllInfo'></div>");
+	$("#videoControll").append("<span id='controllInfo'></span>");
 	
 	// 영상위치 이동  
 	$("#controll span").append("<button id='moveSeek' type='button' class='glyphicon glyphicon-forward btn btn-default active'></button>");
@@ -430,13 +430,13 @@ function video(){
 		
 		// 자막 글씨 크기
 		$("#controll span").append("<button id='fontSize' type='button' class='glyphicon glyphicon-text-size btn btn-default'></button>");
-		$("#controllInfo").append("<div id='cfs'>자막크기 : " + cfs + "</div>");
+		$("#controllInfo").append("<span id='cfs'>자막크기 : " + cfs + "</span>");
 		// 자막위치 
 		$("#controll span").append("<button id='subTop' type='button' class='glyphicon glyphicon-sort-by-alphabet btn btn-default'></button>");
-		$("#controllInfo").append("<div id='cst'>자막위치 : " + cst + "</div>");
+		$("#controllInfo").append("<span id='cst'>자막위치 : " + cst + "</span>");
 		// 자막싱크 
 		$("#controll span").append("<button id='subSync' type='button' class='glyphicon glyphicon-text-width btn btn-default'></button>");
-		$("#controllInfo").append("<div id='css'>자막싱크 : " + css + "</div>");
+		$("#controllInfo").append("<span id='css'>자막싱크 : " + css + "</span>");
 	}
 	
 	$("#controll div").append("<button id='vcMinus' type='button' class='glyphicon glyphicon-minus btn btn-default'></button>");
